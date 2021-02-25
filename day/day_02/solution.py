2ecced6b4400f8ee12c3694a0e2d4c90ee641e2f
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2021-02-26 00:07:35 +0300

    feat(day_2): part 1 and 2 solution

---
+++ day/day_02/solution.py
@@ -0,0 +1,45 @@
+from functools import reduce
+
+def part_1(input):
+    lines = input.split("\n")
+    paper = 0
+
+    for line in lines:
+        if len(line) == 0:
+            continue
+
+        ds = list(map(lambda d: int(d), line.split("x")))
+
+        sides = [ds[0] * ds[1], ds[1] * ds[2], ds[0] * ds[2]]
+
+        m = reduce(lambda a, b: min(a, b), sides, sides[0])
+
+        paper += reduce(lambda a, b: a + b * 2, sides, 0) + m
+
+    return paper
+
+def part_2(input):
+    lines = input.split("\n")
+    ribbon = 0
+
+    for line in lines:
+        if len(line) == 0:
+            continue
+
+        ds = list(map(lambda d: int(d), line.split("x")))
+
+        bow = reduce(lambda a, b: a * b, ds, 1)
+
+        print(bow)
+
+        ribbon += bow
+
+        m = reduce(lambda a, b: min(a, b), ds, ds[0])
+
+        ds.remove(m)
+
+        m2 = reduce(lambda a, b: min(a, b), ds, ds[0])
+
+        ribbon += m2 + m2 + m + m
+
+    return ribbon
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import unittest
 import os
 
-from day.day_01.solution import part_1, part_2
+from day.day_01.solution import part_1 as day_1_part_1, part_2 as day_1_part_2
+from day.day_02.solution import part_1 as day_2_part_1, part_2 as day_2_part_2
 
 INPUT = os.environ["INPUT"]
 
@@ -17,13 +18,13 @@ class day_01_test(unittest.TestCase):
         cases = zip(inputs, outputs)
 
         for case in cases:
-            self.assertEqual(part_1(case[0]), case[1])
+            self.assertEqual(day_1_part_1(case[0]), case[1])
 
         if not INPUT:
             return
 
         input = read_input("day/day_01/input/part_1")
-        output = part_1(input)
+        output = day_1_part_1(input)
 
         print("\n")
         print("input")
@@ -38,13 +39,56 @@ class day_01_test(unittest.TestCase):
         cases = zip(inputs, outputs)
 
         for case in cases:
-            self.assertEqual(part_2(case[0]), case[1])
+            self.assertEqual(day_1_part_2(case[0]), case[1])
 
         if not INPUT:
             return
 
         input = read_input("day/day_01/input/part_2")
-        output = part_2(input)
+        output = day_1_part_2(input)
+
+        print("\n")
+        print("input")
+        print(input)
+        print("output")
+        print(output)
+
+class day_02_test(unittest.TestCase):
+    def test_part_1(self):
+        inputs = ["2x3x4", "1x1x10"]
+        outputs = [58, 43]
+
+        cases = zip(inputs, outputs)
+
+        for case in cases:
+            self.assertEqual(day_2_part_1(case[0]), case[1])
+
+        if not INPUT:
+            return
+
+        input = read_input("day/day_02/input/part_1")
+        output = day_2_part_1(input)
+
+        print("\n")
+        print("input")
+        print(input)
+        print("output")
+        print(output)
+
+    def test_part_2(self):
+        inputs = ["2x3x4", "1x1x10"]
+        outputs = [34, 14]
+
+        cases = zip(inputs, outputs)
+
+        for case in cases:
+            self.assertEqual(day_2_part_2(case[0]), case[1])
+
+        if not INPUT:
+            return
+
+        input = read_input("day/day_02/input/part_2")
+        output = day_2_part_2(input)
 
         print("\n")
         print("input")
